499d07ab0fc43314b93e1699abc0c28bb61c0f79
Rajin Al Maruf <rajinalmarufananda@gmail.com>
2024-11-28 23:25:26 +0600

    basic carousel colpleted

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react'
 import Carousel from './components/Carousel';
-import { SlArrowLeft } from "react-icons/sl";
-import { SlArrowRight } from "react-icons/sl";
 
 
 
@@ -14,34 +12,13 @@ function App() {
     "https://picsum.photos/600/400?random=2",
     "https://picsum.photos/600/400?random=3",
     "https://picsum.photos/600/400?random=4",
-    "https://picsum.photos/600/400?random=5",
+    "https://picsum.photos/600/400?random=5"
   ])
 
 
-  const [currentIndex, setCurrentIndex] = useState(0)
-
-  function handlePrev() {
-    setCurrentIndex(index => {
-     return index === 0? randomImages.length-1 : currentIndex - 1
-    })
-  }
-
-  function handleNext() {
-    setCurrentIndex(index => {
-      return index === randomImages.length-1? 0 : currentIndex + 1
-     })
-  }
-
-
   return (
-    <div className='max-w-xl mx-auto flex mt-14 items-center'>
-      <button onClick={handlePrev} className='bg-gray-700 rounded-full mr-2'>
-        <SlArrowLeft size={30} className='p-2 text-white'/>
-      </button>
-        <Carousel randomImages={randomImages[currentIndex]}/>
-      <button onClick={handleNext} className='bg-gray-700 rounded-full ml-2'>
-        < SlArrowRight size={30} className='p-2 text-white'/>
-      </button>
+    <div>
+        <Carousel randomImages={randomImages}/>
     </div>
   );
 }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
+import { SlArrowLeft } from "react-icons/sl";
+import { SlArrowRight } from "react-icons/sl";
 
 const Carousel = ({randomImages}) => {
+
+  const [currentIndex, setCurrentIndex] = React.useState(0)
+
+  function handlePrev() {
+    setCurrentIndex(index => {
+     return index === 0? randomImages.length-1 : index - 1
+    })
+  }
+
+  function handleNext() {
+    setCurrentIndex(index => {
+      return index === randomImages.length-1? 0 : index + 1
+     })
+  }
+  
   return (
     <div>
-        <div>
-            {/* {randomImages.map(img => (
-                <img src={img}/>
-            ))} */}
-            <img src={randomImages} alt="" />
+        <div className='relative max-w-xl mx-auto mt-14 overflow-hidden flex items-center rounded-xl'>
+            <button onClick={handlePrev} className='bg-gray-800 rounded-full absolute left-0 z-10 ml-2 opacity-60 hover:opacity-80'>
+              <SlArrowLeft size={40} className='p-2 text-white'/>
+            </button>
+            <button onClick={handleNext} className='bg-gray-800 rounded-full absolute right-0 z-10 mr-2 opacity-60 hover:opacity-80'>
+              < SlArrowRight size={40} className='p-2 text-white'/>
+            </button>
+
+            <div className='flex transition-transform ease-out duration-300'
+                style={{transform: `translateX(-${currentIndex*100}%)`}}>
+                  {randomImages.map((image, index) => (
+                    <img key={index} src={image} className="w-full h-auto" alt={`Slide ${index + 1}`}/>
+                  ))}
+            </div>
         </div>
     </div>
   )
